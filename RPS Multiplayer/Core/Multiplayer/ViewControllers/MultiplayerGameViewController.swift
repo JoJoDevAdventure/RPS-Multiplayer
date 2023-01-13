31070f11c42fd88246274a55fe0c109a0f001953
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-01-13 08:21:59 +0100

    +multiplayer game screen

---
+++ RPS Multiplayer/Core/Multiplayer/ViewControllers/MultiplayerGameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MultiplayerGameViewController.swift
+//  RPS Multiplayer
+//
+//  Created by Youssef Bhl on 13/01/2023.
+//
+
+import UIKit
+
+class MultiplayerGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
