5ca1bd62399679de00ae2def3f403b9eed97fd22
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-01-16 10:22:47 +0100

    + Data modification

--- RPS Multiplayer/Data/Data.swift
+++ RPS Multiplayer/Data/Data.swift
@@ -14,6 +14,8 @@ class Data {
     
     public let avatars: [Avatar] = [
         Avatar(image: UIImage(asset: Asset.Images.Avatars.avatar1), mainColor: .magenta, name: "Joshua", hand: Hand(restImage: UIImage(asset: Asset.Images.Hands.H1.rest)!, rockImage: UIImage(asset: Asset.Images.Hands.H1.rock)!, paperImage: UIImage(asset: Asset.Images.Hands.H1.paper)!, scissorsImage: UIImage(asset: Asset.Images.Hands.H1.scissors)!)),
+        Avatar(image: UIImage(asset: Asset.Images.Avatars.avatar1), mainColor: .magenta, name: "Joshua", hand: Hand(restImage: UIImage(asset: Asset.Images.Hands.H1.rest)!, rockImage: UIImage(asset: Asset.Images.Hands.H1.rock)!, paperImage: UIImage(asset: Asset.Images.Hands.H1.paper)!, scissorsImage: UIImage(asset: Asset.Images.Hands.H1.scissors)!)),
+        Avatar(image: UIImage(asset: Asset.Images.Avatars.avatar1), mainColor: .magenta, name: "Joshua", hand: Hand(restImage: UIImage(asset: Asset.Images.Hands.H1.rest)!, rockImage: UIImage(asset: Asset.Images.Hands.H1.rock)!, paperImage: UIImage(asset: Asset.Images.Hands.H1.paper)!, scissorsImage: UIImage(asset: Asset.Images.Hands.H1.scissors)!)),
 
     ]
     
