3698b3bbb0c2542e1d7b23e2a501a9bcb08a3ffc
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-01-17 21:52:12 +0100

    + Ads support

--- RPS Multiplayer/Ressources/AppDelegate.swift
+++ RPS Multiplayer/Ressources/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        DispatchQueue.global().sync {
+            GADMobileAds.sharedInstance().start(completionHandler: nil)
+        }
         return true
     }
 
