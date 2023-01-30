f231f4d10cac07c79e952f503d4f090928c4a60f
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-01-30 15:20:27 +0100

    + DOC

--- RPS Multiplayer/Core/Home/ViewModel/HomeViewModel.swift
+++ RPS Multiplayer/Core/Home/ViewModel/HomeViewModel.swift
@@ -18,19 +18,22 @@ final class HomeViewModel {
     weak var output: HomeViewModelOutput?
     let coordinator = Coordinator.shared
 
-
+    // Output : User did press settings
     public func didPressSettings(vc: UIViewController) {
         coordinator.goToSettingsScreen(from: vc)
     }
     
+    // Output : User did press shop button
     public func didPressShop(vc: UIViewController) {
         coordinator.goToShopScreen(from: vc)
     }
     
+    // Ouput : User did press singlePlayer Button
     public func didPressSinglePlayer(vc: UIViewController) {
         coordinator.goToSinglePlayerScreen(from: vc)
     }
     
+    // OutPut : User did press multiplayer Button
     public func didPressMultiplayer(vc: UIViewController) {
         coordinator.goToMultiplayerScreen(from: vc)
     }
